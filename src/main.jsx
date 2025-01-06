1332f28001605aaedfbe3d1b63cdea191f6bea88
shobankumar06 <shobankumar124@gmail.com>
2025-01-06 11:28:40 +0530

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,10 @@ import { createRoot } from 'react-dom/client'
 import Apps from './Apps.jsx'
 import Hen from './Hen.jsx'
 import Cat from './Cat.jsx'
-import Gallery from './Gallery.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <List/> */}
-    {/* <Apps /> */}
-    <Gallery/>
+ 
+    <Apps />
+   
   </StrictMode>
 )
